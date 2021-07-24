41b2c505267660aa74c62d152e7eb967152bea32
chewyx212 <chewingyx212@gmail.com>
2021-07-25 06:06:59 +0800

    edited create tag

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,4 +1,3 @@
-from django.db.models import query
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -7,7 +6,9 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                mixins.ListModelMixin,
+                mixins.CreateModelMixin,):
     """Manage Tags in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -17,3 +18,6 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
