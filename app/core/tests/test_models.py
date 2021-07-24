9a5823de05d7d9d1533fa55ad25d26fa49a43576
chewyx212 <chewingyx212@gmail.com>
2021-07-25 06:35:28 +0800

    added ingredient and tag baseRecipeAttr

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -51,3 +51,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the tag string represnetation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Potato'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
